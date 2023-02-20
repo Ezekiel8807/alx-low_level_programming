da1c3b75f4b31f09e77d0df61fe8dd0c9e9579ac
Ezekiel8807 <you@example.com>
2023-02-20 06:56:08 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main - a c program thatb print a line to the standard error
+  * Returns 1 (success)
+  */
+int main(void)
+{
+	fput("and that place of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+	return(1);
+}
